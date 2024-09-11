0beb7ac301f4345275d049f4b2de915917ad72f6
Yadidiah Kanaparthi <94169481+YADIDidiah24@users.noreply.github.com>
2024-09-11 20:00:22 +0400

    Add files via upload

---
+++ Real-Time-Chat-Application/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: "https://zcqhfvxodchlnxpgekad.supabase.co/",
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjcWhmdnhvZGNobG54cGdla2FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5MDM4OTgsImV4cCI6MjAzMjQ3OTg5OH0.CGWAlkrBG_KziJWVQhACZZqOjVfwHM3bhegjJm9wZ0w"
+};
